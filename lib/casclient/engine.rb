d62c881af36c1cd0985b6b6f1deb9665ab7ae133
Shannon Snider <shannon@snidersh.org>
2013-06-24 15:13:22 -0500

    Catch error adding duplicate routes

--- lib/casclient/engine.rb
+++ lib/casclient/engine.rb
@@ -5,8 +5,11 @@ require 'rails/engine'
 
     config.to_prepare do
       Rails.application.routes.prepend do
-        get 'logout', :controller => :cas_session, :action => :destroy, :as => :logout
-        get 'cas_proxy_callback/:action', :controller => :cas_proxy_callback, :as => :cas_proxy_callback
+        begin
+          get 'logout', :controller => :cas_session, :action => :destroy, :as => :logout
+          get 'cas_proxy_callback/:action', :controller => :cas_proxy_callback, :as => :cas_proxy_callback
+        rescue
+        end
       end
     end
 
